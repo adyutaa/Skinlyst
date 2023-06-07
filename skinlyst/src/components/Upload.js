570b81de8366f5c971f46b47d7d57b5c8c00507f
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2023-06-08 00:49:38 +0700

    card

--- skinlyst/src/components/Upload.js
+++ skinlyst/src/components/Upload.js
@@ -25,17 +25,32 @@ const Upload = () => {
         <div className="text-center p-3">
           <h2>UPLOAD FOTO WAJAH</h2>
 
-          <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        {/* <Button variant="primary">Go somewhere</Button> */}
-      </Card.Body>
-    </Card>
+             <div className="Card">
+             <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                     <Card.Body>
+                             <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                       Alwi cangkeman asui
+                                    </Card.Text>
+                                    {/* <Button variant="primary">Go somewhere</Button> */}
+                        </Card.Body>
+                      </Card>
+                  </div>
+                  <div className="float: left">
+                  <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                     <Card.Body>
+                             <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                       Alwi cangkeman asui
+                                    </Card.Text>
+                                    {/* <Button variant="primary">Go somewhere</Button> */}
+                        </Card.Body>
+            </Card>
+                  </div>
+                  
+                  
         </div>
       </div>
     </div>
