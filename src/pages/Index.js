15ceac26dd0c0019de467ed00ad0ab5216dc492e
ngalwi <21523247@students.uii.ac.id>
2023-06-07 17:29:57 +0700

    Lagi 1 Page rung nambah

---
+++ src/pages/Index.js
@@ -0,0 +1,4 @@
+import Start from "./Start";
+import Upload from "../components/Upload";
+
+export {Start,Upload}
---
+++ src/pages/Upload.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.css';
+// import {Button} from 'react-bootstrap';
+
+
+const Upload = () => {
+  return (
+    <div className="gradient-bg">
+        <h1> Hello World</h1>
+    </div>
+  );
+}
+
+export default Upload;
