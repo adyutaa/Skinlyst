f2e5ec6cd55048ffd5be7326701bc0a79a025a70
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2023-06-07 21:48:42 +0700

    react page

---
+++ skinlyst/src/components/Start.js
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from './logo.svg';
+import './style.css';
+import {Button} from 'react-bootstrap';
+
+
+const Start = () => {
+  return (
+    <div className='gradient-bg'>
+          <div className='p-4'>
+              <div className='d-flex justify-content-end'>
+                  <h1>Skinlyst</h1></div>
+
+    </div>
+    <div className='text-center'>
+      <h1 className='fw-bold'>KNOW YOUR SKIN BETTER</h1>
+      <h1>SKINCARE ANALYZER</h1>
+    </div>
+    <div className='mt-4'>
+      <img src={logo} className='app-logo mx-auto d-block' alt="logo" fluid />
+    </div>
+    <div>
+      <p className='fw-bold text-center mt-4'>
+      Cek permasalah kulit & kebutuhan skincare untuk kulitmu di Skinlyst!
+      </p>
+    </div>
+    <div className='d-grid col-2 mx-auto mt-4'>
+    <Button className='custom-button fw-bold' href='./Upload' size='lg'>Mulai</Button>{''}
+    </div>
+  </div>
+  );
+}
+
+export default Start;
