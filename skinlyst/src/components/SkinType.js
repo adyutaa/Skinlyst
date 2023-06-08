cb81f164c0aabc36e900ebd3bcc365440861237a
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2023-06-08 08:41:27 +0700

    nambah page tipe kulit, tapi masih error

--- skinlyst/src/components/SkinType.js
+++ skinlyst/src/components/SkinType.js
@@ -1,8 +1,7 @@
 import React from "react";
 import './style.css';
-import { Container, Navbar, Card } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-
+import {Container,Navbar,Card} from 'react-bootstrap';
+import { Button } from 'react-bootstrap'; 
 
 const Upload = () => {
   return (
@@ -24,15 +23,14 @@ const Upload = () => {
 
       <div className="mid">
         <div className="text-center p-3">
-          <h2>UPLOAD FOTO WAJAH</h2>
+          <h2>PILIH JENIS KULITMU</h2>
 
              <div className="Card">
              <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="holder.js/100px180" />
                      <Card.Body>
-                             <Card.Title>Alwi asuog</Card.Title>
+                             <Card.Title>Kering</Card.Title>
                                     <Card.Text>
-                                       Alwi cangkeman asui
                                     </Card.Text>
                                     {/* <Button variant="primary">Go somewhere</Button> */}
                         </Card.Body>
@@ -42,18 +40,23 @@ const Upload = () => {
                   <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="holder.js/100px180" />
                      <Card.Body>
-                             <Card.Title>Alwi </Card.Title>
-                                    <Card.Text>
-                                       Alwi cangkeman asui
-                                    </Card.Text>
+                             <Card.Title>Berminyak</Card.Title>
                                     {/* <Button variant="primary">Go somewhere</Button> */}
                         </Card.Body>
             </Card>
                   </div>
-                  
-                  <div className='d-grid col-2 mx-auto mt-4'>
-                      <Button className='custom-button fw-bold' href='./SkinType' size='lg'>Selanjutnya</Button>{''}
+                  <div className="float: left">
+                  <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                     <Card.Body>
+                             <Card.Title>Normal</Card.Title>
+                                    {/* <Button variant="primary">Go somewhere</Button> */}
+                        </Card.Body>
+            </Card>
                   </div>
+
+                  <button>Selanjutnya</button>
+                  
                   
         </div>
       </div>
