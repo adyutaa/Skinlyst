6fcea8bfc564452d3325aa07e638f2181c86c35b
ngalwi <21523247@students.uii.ac.id>
2023-06-13 14:56:45 +0700

    benerin navbar di market pages

--- skinlyst/src/App.js
+++ skinlyst/src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 import StartPages from './pages/StartPages';
 import UploadPages from './pages/UploadPage';  
 import SkinTypePages from './pages/SkinTypePages';  
-
+import MarketPages from './pages/MarketPages'; 
 
 const App = () => {
   return (
@@ -12,6 +12,7 @@ const App = () => {
         <Route path='/' element={<StartPages />} />
         <Route path='/upload' element={<UploadPages />} />
         <Route path='/tipeKulit' element={<SkinTypePages />} />
+        <Route path='/market' element={<MarketPages />} />
       </Routes>
     </Router>
   )
