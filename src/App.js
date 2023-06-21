a0964194b8cbfb93f8cd6d3f9d786bcc14d42fad
ngalwi <21523247@students.uii.ac.id>
2023-06-21 22:55:31 +0700

    nambah page kondisi kulit & Slider (tapi masih kurang rapi)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import StartPages from './pages/StartPages';
 import UploadPages from './pages/UploadPage';  
 import SkinTypePages from './pages/SkinTypePages';  
 import MarketPages from './pages/MarketPages'; 
+import SdrKeringPgs from './pages/SdrKeringPgs'; 
+import SdrBerminyakPgs from './pages/SdrBerminyakPgs';
+import KondisiPages from './pages/KondisiPages';
 
 const App = () => {
   return (
@@ -13,6 +16,9 @@ const App = () => {
         <Route path='/upload' element={<UploadPages />} />
         <Route path='/tipeKulit' element={<SkinTypePages />} />
         <Route path='/market' element={<MarketPages />} />
+        <Route path='/ukur-kering' element={<SdrKeringPgs />} />
+        <Route path='/ukur-minyak' element={<SdrBerminyakPgs />} />
+        <Route path='/kondisiKulit' element={<KondisiPages />} />
       </Routes>
     </Router>
   )
---
+++ src/pages/KondisiPages.js
@@ -0,0 +1,9 @@
+import React from "react";
+import KondisiKulit from "../components/KondisiKulit";
+
+const KondisiPages = () => {
+    return (
+        <KondisiKulit />
+    );
+}
+export default KondisiPages;
