49c1a9b858e03eb3704e6fccc406576fef772fa8
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2023-06-28 23:53:28 +0700

    nambahin backend(not connected yet) dan rapihin file 2

--- src/pages/Index.js
+++
@@ -1,5 +0,0 @@
-import Start from "./StartPages";
-import Upload from "../components/Upload";
-
-
-export {Start,Upload}
--- src/pages/KandunganPgs.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Kandungan from "../components/Kandungan";
-
-const kandunganPgs = () => {
-    return (
-        <Kandungan />
-    );
-}
-export default kandunganPgs;
