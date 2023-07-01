05d102b5c2be7c0bc0a7cb056a2fd13a3ba0d1db
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2023-07-01 20:41:08 +0700

    .map function solve!

--- frontend/src/components/Market2.jsx
+++ frontend/src/components/Market2.jsx
@@ -1,16 +1,15 @@
-import React, { useEffect, useReducer } from 'react';
-import axios from 'axios';
-import './Market2.css';
-import logger from 'use-reducer-logger';
+import { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
-// import data from '.././models/data';
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+// import data from '../data';
 
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
-      return { ...state, loading: true, error: '' };
+      return { ...state, loading: true };
     case 'FETCH_SUCCESS':
-      return { ...state, loading: false, products: action.payload };
+      return { ...state, products: action.payload, loading: false };
     case 'FETCH_FAIL':
       return { ...state, loading: false, error: action.payload };
     default:
@@ -19,42 +18,40 @@ const reducer = (state, action) => {
 };
 
 function Market2() {
- // const [products, setProducts] = useState([]);
-  const [{ error, products }, dispatch] = useReducer(logger(reducer), {
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
     loading: true,
     error: '',
-});
+  });
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
         const result = await axios.get('/api/products');
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
-      } catch (error) {
-        dispatch({ type: 'FETCH_FAIL', payload: error.message });
-        return;
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
       }
-      
-    //  setProducts(result.data);
+
+      // setProducts(result.data);
     };
     fetchData();
-  }, [error.message]);
-
+  }, []);
   return (
     <div>
-      <header>
-        <Link to='/'>
-          <h1 className='fw-bold'>Skinlyst</h1>
-        </Link>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-        {products ? (
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
           products.map((product) => (
             <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`} />
-              <img src={product.image} alt={product.name} />
+              <Link to={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </Link>
               <div className="product-info">
                 <Link to={`/product/${product.slug}`}>
                   <p>{product.name}</p>
@@ -66,13 +63,9 @@ function Market2() {
               </div>
             </div>
           ))
-        ) : (
-          <div>No products available</div>
         )}
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
-
 export default Market2;
---
+++ frontend/src/components/productComponent.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+      case 'FETCH_REQUEST':
+        return { ...state, loading: true, error: '' };
+      case 'FETCH_SUCCESS':
+        return { ...state, loading: false, products: action.payload };
+      case 'FETCH_FAIL':
+        return { ...state, loading: false, error: action.payload };
+      default:
+        return state;
+    }
+};
+  
+function ProductScreen(props) {
+    const { product } = props;
+    const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+        loading: true,
+        error: '',
+    });
+      useEffect(() => {
+        const fetchData = async () => {
+          dispatch({ type: 'FETCH_REQUEST' });
+          try {
+            const result = await axios.get('/api/products');
+            dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+          } catch {
+            dispatch({ type: 'FETCH_FAIL', payload: error.message });
+            return;
+          }
+          
+        //  setProducts(result.data);
+        };
+        fetchData();
+      }, []);
+    return (
+      
+    <Card key={product.slug}>
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Card.Text>
+          <strong>${product.price}</strong>
+        </Card.Text>
+        <Button>Add to Cart</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ProductScreen;
