7d7652421eb38b43b29190057abb9da86e81e16f
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2023-07-03 22:58:14 +0700

    dashboard added, masih burik

--- frontend/src/pages/KondisiPages.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import KondisiKulit from "../components/KondisiKulit";
-
-const KondisiPages = () => {
-    return (
-        <KondisiKulit />
-    );
-}
-export default KondisiPages;
--- frontend/src/pages/Market2Pages.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Market2 from "../components/Market2";
-
-const Market2Pages = () => {
-    return (
-        <Market2 />
-    );
-}
-export default Market2Pages;
